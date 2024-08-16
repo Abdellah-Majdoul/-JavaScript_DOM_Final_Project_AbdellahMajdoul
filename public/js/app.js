32b048cc11c98615d25075e0d28fc3dc17460a2e
Abdellah-Majdoul <abdellahmajdoul57@gmail.com>
2024-08-16 10:14:17 +0100

    modal

--- public/js/app.js
+++ public/js/app.js
@@ -15,3 +15,67 @@ navLinks.forEach((link, index) => {
             table[index].classList.add('show', 'active','fade');
         });
     });
+
+    
+
+const modal = document.getElementById("myModal");
+const openModalBtn = document.getElementById("openModal");
+const closeBtn = document.querySelector(".close");
+const closeModalBtn = document.getElementById("closeModalBtn");
+
+
+openModalBtn.addEventListener("click", (e)=> {
+    e.preventDefault()
+    modal.style.display = "flex";
+});
+
+
+closeBtn.addEventListener("click", ()=> {
+    modal.style.display = "none";
+});
+
+closeModalBtn.addEventListener("click", ()=> {
+    modal.style.display = "none";
+});
+
+
+
+
+let containers = document.querySelectorAll(".carousel-container");
+let currentIndex = 0;
+
+const slideImage = (index, container) => {
+    let slides = container.querySelectorAll(".cardCarousel");
+    let carosel = container.querySelector(".carousel");
+    let slideWidth = slides[0].clientWidth;
+    let indicators = container.querySelectorAll('.indicator');
+
+   
+    if (index < 0) {
+        index = slides.length - 1;
+    } else if (index >= slides.length) {
+        index = 0;
+    }
+    indicators.forEach(indicator => {
+        indicator.classList.remove('activeIndicator');
+    });
+    indicators[index].classList.add("activeIndicator"); 
+    carosel.style.transform = `translateX(-${slideWidth * index}px)`;
+    currentIndex = index;
+};
+containers.forEach(container => {
+    let slides = container.querySelectorAll(".cardCarousel");
+    let indicatorsGrp = document.createElement("div");
+    indicatorsGrp.classList.add("indicators-grp");
+    container.appendChild(indicatorsGrp);
+
+    slides.forEach((slide, i) => {
+        let indicator = document.createElement("div");
+        indicator.classList.add("indicator");
+        indicatorsGrp.appendChild(indicator);      
+        indicator.addEventListener("click", () => {
+            slideImage(i, container);
+        });
+    });
+    indicatorsGrp.querySelector(".indicator").classList.add('activeIndicator');   
+});
